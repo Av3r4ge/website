b3a09f1b5ea21d704ed62e2feab1f9c7551869dd
avg <heyoitsflowey@gmail.com>
2024-12-05 23:59:28 -0500

    tabs and hero work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,57 @@
-import { WavyBackground } from "@/components/ui/wavy-background"
 import { HeroText } from "@/components/hero-text"
 import { BackgroundBeamsWithCollision } from "@/components/ui/exploding-beams"
+import { Tabs } from "@/components/ui/tabs"
+import { BackgroundBeams } from "@/components/ui/background-beams"
 
 export default function Home() {
-  return (
-  <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto">
+  const tabs = [
+    {
+      title: "About",
+      value: "about",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-neutral-800">
+          <p className="text-xl md:text-4xl font-bold">About me</p>
+        </div>
+      ),
+    },
+    {
+      title: "Projects",
+      value: "projects",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-white bg-neutral-800">
+          <p className="text-xl md:text-4xl font-bold">Projects</p>
+        </div>
+      )
+    },
+    {
+      title: "Skills",
+      value: "skills",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-neutral-800">
+          <p>Skills</p>
+        </div>
+      )
+    }
+    
+  ]
 
-      <BackgroundBeamsWithCollision className="relative flex items-center w-full justify-center overflow-hidden" children={<HeroText/>}></BackgroundBeamsWithCollision>
+  return (
+  <main className="relative overflow-hidden mx-auto  bg-neutral-950">
+
+
+      <BackgroundBeamsWithCollision className="relative flex items-center w-full justify-center overflow-hidden">
+        { <HeroText></HeroText> }
+      </BackgroundBeamsWithCollision>
+
+      <div className="relative h-150 pb-20">
+
+        <div id="tabs"  className="h-[20rem] md:h-[50rem] [perspective:1000px] relative b flex flex-col max-w-7xl mx-auto w-full items-start justify-start my-20">
+          <Tabs tabs={tabs} />
+        </div>
+
+      </div>
+
+
 
   </main>
   )
--- src/components/hero-text.tsx
+++ src/components/hero-text.tsx
@@ -1,24 +1,33 @@
+'use client'
+
 import React from "react";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import Link from "next/link";
 
 export function HeroText() {
+
+    const scrolltoHash = function (element_id: string) {
+        const element = document.getElementById(element_id)
+        element?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    }
+    
     return (
         <div className="items-center text-center">
-            <h2 className="lg:text-7xl md:text-6xl text-5xl font-bold text-white"> Hey, I'm <span> Alex </span> </h2>
-            <TextGenerateEffect duration={1} className="pt-2" words={"And I'm driven by a passion for all things IT"}/>
+            <h2 className="lg:text-7xl md:text-6xl text-5xl font-bold text-white"> Hey, I&apos;m <span> Alex </span> </h2>
+            <TextGenerateEffect className="pt-2" words={"And I'm driven by a passion for all things I.T."}/>
 
             <div className="mt-10 w-full items-center">
                 <button className="p-[3px] relative mr-5">
                     <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-                    <div className="px-8 py-2 bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                        <p className="text-2xl"> Projects </p>
+                    <div className="px-8 py-2 bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent" onClick={() => scrolltoHash("tabs")}>
+                        <a className="text-2xl"> Projects </a>
                     </div>
                 </button>
 
                 <button className="p-[3px] relative">
                     <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                     <div className="px-8 py-2 bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                        <a href="https://github.com/av3r4ge" className="text-2xl"> Github </a>
+                        <Link href="https://github.com/av3r4ge" className="text-2xl"> Github </Link>
                     </div>
                 </button>
             </div>
