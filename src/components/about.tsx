173c21932bdaed8895b795798f78d7d1395b0c9f
avg <heyoitsflowey@gmail.com>
2024-12-08 16:57:38 -0500

    escape apostrophes

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,10 +5,10 @@ export function About() {
             <hr/>
 
             <p className="my-6 text-white text-3xl text-wrap">
-                Hey there, my name is Alex. I'm currently studying towards a career in <b>IT Networking and Security</b>. I have a passion for learning new things and enjoy the creative challenge of solving problems.
+                Hey there, my name is Alex. I&apos;m currently studying towards a career in <b>IT Networking and Security</b>. I have a passion for learning new things and enjoy the creative challenge of solving problems.
             </p>
             <p className="my-6 text-white text-3xl text-wrap">
-                Programming is a self-taught hobby of mine that I've been pursuing for quite some time. Since 2020, I've been working as a freelancer, creating websites and developing game plugins for various clients."
+                Programming is a self-taught hobby of mine that I&apos;ve been pursuing for quite some time. Since 2020, I&apos;ve been working as a freelancer, creating websites and developing game plugins for various clients."
             </p>
             <p className="my-6 text-white text-3xl text-wrap">
                 My expertise primary lies in backend development. While I am also proficient in frontend development, my strengths are experience are stronger in the backend.
